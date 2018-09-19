f9591c1bd76d489835fa097a89f8290c273e988e
Takayoshi Kimura <takayoshi@gmail.com>
2018-09-19 15:43:51 +0900

    n/a

--- src/main/java/com/example/TestSessionObject.java
+++ src/main/java/com/example/TestSessionObject.java
@@ -3,15 +3,15 @@ package com.example;
 import java.io.Serializable;
 
 public class TestSessionObject implements Serializable {
-    public int id = 2;
-    public String name = "v2";
-    public String data = "foo";
+    public int id = 3;
+    public String name = "v3";
+    public String data3 = "foo3";
     public String data2 = "bar";
     public String toString() {
         return "TestSessionObject("
             + id + ", "
             + name + ", "
-            + data + ", "
+            + data3 + ", "
             + data2 + ")";
     }
 }
